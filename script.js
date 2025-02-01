2678116082429b646ba53a6d4fefc374fc9290d7
TanishkaCh30 <137798637+TanishkaCh30@users.noreply.github.com>
2025-02-01 11:34:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const getSumBtn = document.createElement("button");
+let prices = document.getElementsByClassName("price");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
+	let totalPrice=0;
+	for(let i = 0 ; i<prices.length ; i++){
+		totalPrice+=Number(prices[i].innerText);
+	}
+
+	let newRow = document.createElement("tr");
+	newRow.innerText = totalPrice;
+	document.body.appendChild(newRow);
   
 };
 
